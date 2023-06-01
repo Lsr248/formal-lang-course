80dd389c62db9f4c3baaa71f8539639b5d2f9bb2
Lsr248 <i-fattakhova@mail.ru>
2023-06-01 05:49:12 +0300

    Added fixes to Task7

--- project/rfaUtils.py
+++ project/rfaUtils.py
@@ -4,7 +4,8 @@ from pyformlang.cfg import Variable
 from pyformlang.finite_automaton import DeterministicFiniteAutomaton
 
 from project.ecfgUtils import ECFG
-
+from scipy.sparse import dok_matrix
+import numpy
 
 class RFA:
     """
@@ -31,6 +32,24 @@ def rfa_from_ecfg(ecfg: ECFG):
     for k, v in ecfg.productions.items():
         boxes[k] = v.to_epsilon_nfa().minimize()
     return RFA(start=ecfg.start, boxes=boxes)
+
+def toAdjacencyMatrix(self):
+    """
+    Convert recursive finite automaton to a adjacency matrix.
+    """
+    states = set()
+    for var, dfa in self.dfas.items():
+        for x in dfa.states:
+            states.add((var, x))
+    state_idx = {k: i for i, k in enumerate(states)}
+    result = {}
+    for var, dfa in self.dfas.items():
+        for fro, symb, to in dfa:
+            mat = result.setdefault(
+                symb, dok_matrix((len(state_idx), len(state_idx)), dtypeb= numpy.bool_)
+            )
+            mat[state_idx[(var, fro)], state_idx[(var, to)]] = True
+    return result, state_idx
 
 
 def minimize(rfa:RFA):
